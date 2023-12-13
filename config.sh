b62be80bd9a1562f1224d6ab300022de516883d4
정시현 <54366260+jungsiroo@users.noreply.github.com>
2023-12-13 16:53:33 +0000

    Update Post 2023-12-13 16:53:12

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file="_config.yml"
+line_number=113
+
+if [ "$1" == "deploy" ]; then
+    sed -i "${line_number}s/^/#/" "$file"
+    echo "DEPLOY JOB DONE"
+elif [ "$1" == "server" ]; then
+    sed -i "${line_number}s/^#//" "$file"
+    echo "SERVER JOB DONE"
+else
+    echo "Usage: $0 [deploy | server]"
+fi
